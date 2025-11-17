36839c2186198da5fb9358cb4e8b82ab0cfe3195
Franson33 <antonchikfr@gmail.com>
2025-11-17 23:06:59 +0200

    added process registry module, which will provide the usage of the via tuple

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -54,8 +54,12 @@ end
 defmodule Todo.DatabaseWorker do
   use GenServer
 
-  def start_link(db_folder) do
-    GenServer.start_link(__MODULE__, db_folder)
+  def start_link({db_folder, worker_id}) do
+    GenServer.start_link(
+      __MODULE__,
+      db_folder,
+      name: via_tuple(worker_id)
+    )
   end
 
   def init(db_folder) do
@@ -66,12 +70,12 @@ defmodule Todo.DatabaseWorker do
     {:ok, db_folder}
   end
 
-  def store(pid, key, data) do
-    GenServer.cast(pid, {:store, key, data})
+  def store(worker_id, key, data) do
+    GenServer.cast(via_tuple(worker_id), {:store, key, data})
   end
 
-  def get(pid, key) do
-    GenServer.call(pid, {:get, key})
+  def get(worker_id, key) do
+    GenServer.call(via_tuple(worker_id), {:get, key})
   end
 
   def handle_cast({:store, key, data}, db_folder) do
@@ -101,5 +105,9 @@ defmodule Todo.DatabaseWorker do
 
   defp file_name(key, db_folder) do
     Path.join(db_folder, to_string(key))
+  end
+
+  defp via_tuple(worker_id) do
+    Todo.ProcessRegistry.via_tuple({__MODULE__, worker_id})
   end
 end
