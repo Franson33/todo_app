433b10cf9acb1931ecc307fb8c6ad3c9992eff55
Franson33 <antonchikfr@gmail.com>
2025-12-01 22:07:56 +0200

    refactored the server module to use Agent instead of GenServer

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,58 +1,57 @@
 defmodule Todo.Server do
-  use GenServer, restart: :temporary
-
-  def init({list_name, initial_entries}) do
-    IO.puts("Strting to-do server for #{list_name}.")
-
-    {:ok, {list_name, initial_entries}, {:continue, :init}}
-  end
-
-  def handle_continue(:init, {list_name, initial_entries}) do
-    (Todo.Database.get(list_name) || Todo.List.new(initial_entries))
-    |> then(&{:noreply, {list_name, &1}})
-  end
+  use Agent, restart: :temporary
 
   def start_link({list_name, initial_entries}) do
-    GenServer.start_link(__MODULE__, {list_name, initial_entries}, name: via_tuple(list_name))
+    Agent.start_link(
+      fn ->
+        IO.puts("Starting todo server for #{list_name}")
+
+        (Todo.Database.get(list_name) || Todo.List.new(initial_entries))
+        |> then(&{list_name, &1})
+      end,
+      name: via_tuple(list_name)
+    )
   end
 
-  def entries(pid, date) do
-    GenServer.call(pid, {:entries, date})
+  def entries(todo_server, date) do
+    Agent.get(
+      todo_server,
+      fn {_name, todo_list} ->
+        Todo.List.entries(todo_list, date)
+      end
+    )
   end
 
-  def add_entry(pid, entry) do
-    GenServer.cast(pid, {:add_entry, entry})
+  def add_entry(todo_server, new_entry) do
+    Agent.cast(
+      todo_server,
+      fn {name, todo_list} ->
+        Todo.List.add_entry(todo_list, new_entry)
+        |> tap(&Todo.Database.store(name, &1))
+        |> then(&{name, &1})
+      end
+    )
   end
 
-  def update_entry(pid, id, updater) do
-    GenServer.cast(pid, {:update_entry, id, updater})
-  end
+  # def update_entry(pid, id, updater) do
+  #  GenServer.cast(pid, {:update_entry, id, updater})
+  # end
 
-  def delete_entry(pid, id) do
-    GenServer.cast(pid, {:delete_entry, id})
-  end
+  # def delete_entry(pid, id) do
+  #  GenServer.cast(pid, {:delete_entry, id})
+  # end
 
-  def handle_call({:entries, date}, _from, {name, list}) do
-    {:reply, Todo.List.entries(list, date), {name, list}}
-  end
+  # def handle_cast({:update_entry, id, updater}, {list_name, list}) do
+  #  Todo.List.update_entry(list, id, updater)
+  #  |> tap(&Todo.Database.store(list_name, &1))
+  #  |> then(&{:noreply, {list_name, &1}})
+  # end
 
-  def handle_cast({:add_entry, entry}, {list_name, list}) do
-    Todo.List.add_entry(list, entry)
-    |> tap(&Todo.Database.store(list_name, &1))
-    |> then(&{:noreply, {list_name, &1}})
-  end
-
-  def handle_cast({:update_entry, id, updater}, {list_name, list}) do
-    Todo.List.update_entry(list, id, updater)
-    |> tap(&Todo.Database.store(list_name, &1))
-    |> then(&{:noreply, {list_name, &1}})
-  end
-
-  def handle_cast({:delete_entry, id}, {list_name, list}) do
-    Todo.List.delete_entry(list, id)
-    |> tap(&Todo.Database.store(list_name, &1))
-    |> then(&{:noreply, {list_name, &1}})
-  end
+  # def handle_cast({:delete_entry, id}, {list_name, list}) do
+  #  Todo.List.delete_entry(list, id)
+  #  |> tap(&Todo.Database.store(list_name, &1))
+  #  |> then(&{:noreply, {list_name, &1}})
+  # end
 
   defp via_tuple(name) do
     Todo.ProcessRegistry.via_tuple({__MODULE__, name})
