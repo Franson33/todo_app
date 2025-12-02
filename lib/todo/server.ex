4cedc85deaf973a3f05dbad9fe18329a7d3ab5e3
Franson33 <antonchikfr@gmail.com>
2025-12-02 22:04:54 +0200

    refactored update and delete functions to agent

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -33,25 +33,27 @@ defmodule Todo.Server do
     )
   end
 
-  # def update_entry(pid, id, updater) do
-  #  GenServer.cast(pid, {:update_entry, id, updater})
-  # end
+  def update_entry(todo_server, id, updater) do
+    Agent.cast(
+      todo_server,
+      fn {name, todo_list} ->
+        Todo.List.update_entry(todo_list, id, updater)
+        |> tap(&Todo.Database.store(name, &1))
+        |> then(&{name, &1})
+      end
+    )
+  end
 
-  # def delete_entry(pid, id) do
-  #  GenServer.cast(pid, {:delete_entry, id})
-  # end
-
-  # def handle_cast({:update_entry, id, updater}, {list_name, list}) do
-  #  Todo.List.update_entry(list, id, updater)
-  #  |> tap(&Todo.Database.store(list_name, &1))
-  #  |> then(&{:noreply, {list_name, &1}})
-  # end
-
-  # def handle_cast({:delete_entry, id}, {list_name, list}) do
-  #  Todo.List.delete_entry(list, id)
-  #  |> tap(&Todo.Database.store(list_name, &1))
-  #  |> then(&{:noreply, {list_name, &1}})
-  # end
+  def delete_entry(todo_server, id) do
+    Agent.cast(
+      todo_server,
+      fn {name, todo_list} ->
+        Todo.List.delete_entry(todo_list, id)
+        |> tap(&Todo.Database.store(name, &1))
+        |> then(&{name, &1})
+      end
+    )
+  end
 
   defp via_tuple(name) do
     Todo.ProcessRegistry.via_tuple({__MODULE__, name})
