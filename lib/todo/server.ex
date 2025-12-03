1e242513b9ce33b8edaa6819729507a99d683cd4
Franson33 <antonchikfr@gmail.com>
2025-12-03 22:17:40 +0200

    added self termination to each todo server process

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,6 +1,8 @@
 defmodule Todo.Server do
   use GenServer, restart: :temporary
 
+  @expiry_idle_timeout :timer.seconds(10)
+
   def init({list_name, initial_entries}) do
     IO.puts("Strting to-do server for #{list_name}.")
 
@@ -9,7 +11,13 @@ defmodule Todo.Server do
 
   def handle_continue(:init, {list_name, initial_entries}) do
     (Todo.Database.get(list_name) || Todo.List.new(initial_entries))
-    |> then(&{:noreply, {list_name, &1}})
+    |> then(&{:noreply, {list_name, &1}, @expiry_idle_timeout})
+  end
+
+  def handle_info(:timeout, {list_name, list}) do
+    IO.puts("Stopping todo server for #{list_name}")
+
+    {:stop, :normal, {list_name, list}}
   end
 
   def start_link({list_name, initial_entries}) do
@@ -33,25 +41,25 @@ defmodule Todo.Server do
   end
 
   def handle_call({:entries, date}, _from, {name, list}) do
-    {:reply, Todo.List.entries(list, date), {name, list}}
+    {:reply, Todo.List.entries(list, date), {name, list}, @expiry_idle_timeout}
   end
 
   def handle_cast({:add_entry, entry}, {list_name, list}) do
     Todo.List.add_entry(list, entry)
     |> tap(&Todo.Database.store(list_name, &1))
-    |> then(&{:noreply, {list_name, &1}})
+    |> then(&{:noreply, {list_name, &1}, @expiry_idle_timeout})
   end
 
   def handle_cast({:update_entry, id, updater}, {list_name, list}) do
     Todo.List.update_entry(list, id, updater)
     |> tap(&Todo.Database.store(list_name, &1))
-    |> then(&{:noreply, {list_name, &1}})
+    |> then(&{:noreply, {list_name, &1}, @expiry_idle_timeout})
   end
 
   def handle_cast({:delete_entry, id}, {list_name, list}) do
     Todo.List.delete_entry(list, id)
     |> tap(&Todo.Database.store(list_name, &1))
-    |> then(&{:noreply, {list_name, &1}})
+    |> then(&{:noreply, {list_name, &1}, @expiry_idle_timeout})
   end
 
   defp via_tuple(name) do
